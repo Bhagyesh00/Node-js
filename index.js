539098cea49fa5db83d398050fea321cc105d964
Bhagyesh khachane <khachanebhagyesh7@gmail.com>
2023-11-22 14:09:53 +0530

    a

--- index.js
+++ index.js
@@ -40,53 +40,54 @@ app.post("/webhook",(req,res)=>{ //i want some
 
     console.log(JSON.stringify(body_param,null,2));
 
-    if(body_param.object){
-        console.log("inside body param");
-        if (
-            body_param.entry &&
-            body_param.entry[0].changes &&
-            body_param.entry[0].changes[0].value.metadata &&
-            body_param.entry[0].changes[0].value.messages &&
-            body_param.entry[0].changes[0].value.messages[0]
-          ) {
-            let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-          
-            console.log("phone number " + phon_no_id);
-            console.log("from " + from);
-            console.log("body param " + msg_body);
-          
-            axios({
-              method: "POST",
-              url:
-                "https://graph.facebook.com/v17.0/" +
-                phon_no_id +
-                "/messages?access_token=" +
-                token,
-              headers: {
-                    "Content-Type": "application/json",
-                  },
-              data: {
-                messaging_product: "whatsapp",
-                to: from,
-                text: {
-                  body: "Hi.. I'm Wify, your message is " + msg_body,
+    try {
+        if(body_param.object){
+            console.log("inside body param");
+            if (
+                body_param.entry &&
+                body_param.entry[0].changes &&
+                body_param.entry[0].changes[0].value.metadata &&
+                body_param.entry[0].changes[0].value.messages &&
+                body_param.entry[0].changes[0].value.messages[0]
+            ) {
+                let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+                let from = body_param.entry[0].changes[0].value.messages[0].from;
+                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+            
+                console.log("phone number " + phon_no_id);
+                console.log("from " + from);
+                console.log("body param " + msg_body);
+            
+                axios({
+                method: "POST",
+                url:
+                    "https://graph.facebook.com/v17.0/" +
+                    phon_no_id +
+                    "/messages?access_token=" +
+                    token,
+                headers: {
+                        "Content-Type": "application/json",
+                    },
+                data: {
+                    messaging_product: "whatsapp",
+                    to: from,
+                    text: {
+                    body: "Hi.. I'm Wify, your message is " + msg_body,
+                    },
                 },
-              },
-            })
-              .then(() => {
-                res.status(200).send("Message Sent...");
-              })
-              .catch((error) => {
-                console.error("Error sending message:", error);
-                res.status(500).send("Internal Server Error");
-              });
-          } else {
-            res.status(404).send("Error Occur...");
-          }          
+                })
+                .then(() => {
+                    res.status(200).send("Message Sent...");
+                });
+            } else {
+                res.status(404).send("Error Occur...");
+            }          
+        }
+        res.status(200).send("Empty request...");
+    } catch (error) {
+        console.error("Error sending message:", error);
+        res.status(500).send("Internal Server Error");
     }
-    res.status(200).send("Empty request...");
 });
 
 app.get("/",(req,res)=>{
