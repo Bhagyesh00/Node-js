56bdb9fe2caaca19026afd03e2e4fff094837295
Bhagyesh khachane <khachanebhagyesh7@gmail.com>
2023-11-22 10:59:53 +0530

    change variable body to body_param

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post("/webhook", (req, res) => {
-    let body = req.body;
-    console.log(body);
+    let body_param = req.body;
+    console.log(body_param);
 
-    if (body.object) {
-        if (body.entry && body.entry[0].changes && body.entry[0].changes[0].value.messages && body.entry[0].changes[0].value.messages[0]) {
-            let mob = body.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body.entry[0].changes[0].value.messages[0].text_body;
+    if (body_param.object) {
+        if (body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.messages && body_param.entry[0].changes[0].value.messages[0]) {
+            let mob = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = body_param.entry[0].changes[0].value.messages[0].from;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text_body;
 
             console.log(mob);
             console.log(from);
@@ -103,13 +103,13 @@ module.exports = app;
 
 // app.post("/webhook",(req,res)=>{
 
-//     let body = req.body;
+//     let body_param = req.body;
 //     console.log(JSON.stringify(body_param,null,2));
 //     if(body_param.object){
-//         if(body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.message && body_param.entry[0].changes[0].value.message[0]){
-//             let mob = body.entry[0].changes[0].value.metadata.phone_number_id;
-//             let from =  body.entry[0].changes[0].value.message[0].from;
-//             let msg_body =  body.entry[0].changes[0].value.message[0].text_body;
+//         if(body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.messages && body_param.entry[0].changes[0].value.messages[0]){
+//             let mob = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+//             let from =  body_param.entry[0].changes[0].value.messages[0].from;
+//             let msg_body =  body_param.entry[0].changes[0].value.messages[0].text_body;
 
 //             axios({
 
