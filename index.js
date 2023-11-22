8afedf2d051f7a1ade09b513c687a4b3d75f7ee1
Bhagyesh khachane <khachanebhagyesh7@gmail.com>
2023-11-22 11:09:38 +0530

    correction

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/webhook', (req, res) => {
 
 app.post("/webhook", (req, res) => {
     let body_param = req.body;
-    console.log(body_param);
+    console.log(JSON.stringify(body_param));
 
     if (body_param.object) {
         if (body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.messages && body_param.entry[0].changes[0].value.messages[0]) {
@@ -39,7 +39,7 @@ app.post("/webhook", (req, res) => {
             console.log(msg_body);
 
             axios({
-                method: 'post',
+                method: 'POST',
                 url: 'https://graph.facebook.com/v17.0/' + mob + '/messages?access_token=' + token,
                 data: {
                     messaging_product: 'whatsapp',
