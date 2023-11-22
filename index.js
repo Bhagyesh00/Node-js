77931d780d516a8113fb1022cdaaf4ea7feefa61
Bhagyesh khachane <khachanebhagyesh7@gmail.com>
2023-11-22 12:36:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,75 +1,87 @@
 // index.js
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const express=require("express");
+const body_parser=require("body-parser");
+const axios=require("axios");
 require('dotenv').config();
 
-const app = express().use(bodyParser.json());
-const token = process.env.TOKEN;
-const myToken = process.env.MYTOKEN;
+const app=express().use(body_parser.json());
 
-app.get('/webhook', (req, res) => {
-    let mode = req.query["hub.mode"];
-    let challenge = req.query["hub.challenge"];
-    let verify_token = req.query["hub.verify_token"];
+const token=process.env.TOKEN;
+const mytoken=process.env.MYTOKEN;//prasath_token
 
-    if (mode && verify_token) {
-        if (mode === 'subscribe' && verify_token === myToken) {
-            res.status(200).send(challenge);
-        } else {
+app.listen(process.env.PORT,()=>{
+    console.log("webhook is listening");
+});
+
+//to verify the callback url from dashboard side - cloud api side
+app.get("/webhook",(req,res)=>{
+   let mode=req.query["hub.mode"];
+   let challange=req.query["hub.challenge"];
+   let token=req.query["hub.verify_token"];
+
+
+    if(mode && token){
+
+        if(mode==="subscribe" && token===mytoken){
+            res.status(200).send(challange);
+        }else{
             res.status(403);
         }
+
     }
-    res.status(200).send("Token Not Verify...");
+
 });
 
-app.post("/webhook", (req, res) => {
-    let body_param = req.body;
-    console.log(JSON.stringify(body_param));
+app.post("/webhook",(req,res)=>{ //i want some 
 
-    if (body_param.object) {
-        if (body_param.entry && body_param.entry[0].changes && body_param.entry[0].changes[0].value.messages && body_param.entry[0].changes[0].value.messages[0]) {
-            let mob = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text_body;
+    let body_param=req.body;
 
-            console.log(mob);
-            console.log(from);
-            console.log(msg_body);
+    console.log(JSON.stringify(body_param,null,2));
 
-            axios({
-                method: 'POST',
-                url: 'https://graph.facebook.com/v17.0/' + mob + '/messages?access_token=' + token,
-                data: {
-                    messaging_product: 'whatsapp',
-                    to: from,
-                    text: {
-                        body: 'Thank You To Contact Wify Systems Private Limited...'
-                    }
-                },
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(404);
-        }
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v17.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Wify, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
+            }
+
     }
-    res.status(200).end();
+
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send("Welcome");
+app.get("/",(req,res)=>{
+    res.status(200).send("hello this is webhook setup");
 });
-
-app.listen(8000, () => {
-    console.log('I am in Home Page');
-});
-
-module.exports = app;
 
 // const express = require('express');
 // const bodyParser = require('body-parser');
